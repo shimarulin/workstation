e97fa5dda8ad0d35a473baa348846dfec8ca41fb
Vyacheslav Shimarulin <shimarulin@gmail.com>
2021-10-06 00:17:31 +0300

    feature(zsh): additional key bindings

---
+++ roles/zsh/files/home/.config/zsh/plugins/key-bindings.plugin.zsh
@@ -0,0 +1,7 @@
+# https://github.com/junegunn/fzf/blob/master/shell/key-bindings.zsh
+# Ctrl+R
+bindkey '^R' fzf-history-widget
+
+# https://github.com/m42e/zsh-histdb-fzf/blob/master/fzf-histdb.zsh
+# Up Arrow
+bindkey '^[OA' histdb-fzf-widget
