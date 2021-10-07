192536ef388fa936a28a25100ac037bbce25fbb6
Vyacheslav Shimarulin <shimarulin@gmail.com>
2021-10-07 17:49:57 +0300

    feature(zsh): remove zsh-histdb-fzf

--- roles/zsh/files/home/.config/zsh/plugins/key-bindings.plugin.zsh
+++
@@ -1,7 +0,0 @@
-# https://github.com/junegunn/fzf/blob/master/shell/key-bindings.zsh
-# Ctrl+R
-bindkey '^R' fzf-history-widget
-
-# https://github.com/m42e/zsh-histdb-fzf/blob/master/fzf-histdb.zsh
-# Up Arrow
-bindkey '^[OA' histdb-fzf-widget
