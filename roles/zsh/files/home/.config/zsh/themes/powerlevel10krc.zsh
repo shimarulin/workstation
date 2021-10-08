9856f1d5a52db7f343351d021b6366104d33d6ae
Vyacheslav Shimarulin <shimarulin@gmail.com>
2021-10-08 12:14:21 +0300

    docs(zsh): detect Linux console

--- roles/zsh/files/home/.config/zsh/themes/powerlevel10krc.zsh
+++ roles/zsh/files/home/.config/zsh/themes/powerlevel10krc.zsh
@@ -1,4 +1,6 @@
 # To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
+
+# Detect Linux console can be possible with "$TERM" = "linux" or `tput colors` = "8"
 if [ `tput colors` = "8" ]; then
   [[ ! -f ~/.config/zsh/.p10k.tty.zsh ]] || source ~/.config/zsh/.p10k.tty.zsh
 else
