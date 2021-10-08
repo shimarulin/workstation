2e6aa298da71199d89dd0af990204be97cef0de7
Vyacheslav Shimarulin <shimarulin@gmail.com>
2021-10-08 11:42:10 +0300

    feature(zsh): tty theme

--- roles/zsh/files/home/.config/zsh/themes/powerlevel10krc.zsh
+++ roles/zsh/files/home/.config/zsh/themes/powerlevel10krc.zsh
@@ -1,2 +1,6 @@
 # To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
-[[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
+if [ `tput colors` = "8" ]; then
+  [[ ! -f ~/.config/zsh/.p10k.tty.zsh ]] || source ~/.config/zsh/.p10k.tty.zsh
+else
+  [[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
+fi
