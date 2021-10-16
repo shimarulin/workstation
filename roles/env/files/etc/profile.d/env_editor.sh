b1b6845aa2f5aa599c8ffba68d1067e9f7ab8a25
Vyacheslav Shimarulin <shimarulin@gmail.com>
2021-10-16 03:16:00 +0300

    fix: move environment variables to profile.d files

---
+++ roles/env/files/etc/profile.d/env_editor.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export EDITOR=/usr/bin/nvim
+export VISUAL=/usr/bin/nvim
---
+++ roles/env/files/etc/profile.d/env_path.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# If user ID is greater than or equal to 1000 &
+# if ~/.local/bin exists and is a directory &
+# if ~/.local/bin is not already in your $PATH
+# then export ~/.local/bin to your $PATH.
+if [[ $UID -ge 1000 && -d $HOME/.local/bin && -z $(echo $PATH | grep -o $HOME/.local/bin) ]]
+then
+    export PATH=$HOME/.local/bin:${PATH}
+fi
