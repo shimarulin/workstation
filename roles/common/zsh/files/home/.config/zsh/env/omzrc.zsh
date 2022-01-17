1a62d02f34c9c2d9be62b7c2b8116b3a687e19f4
Vyacheslav Shimarulin <shimarulin@gmail.com>
2022-01-17 23:14:13 +0300

    feat: add NVM

--- roles/common/zsh/files/home/.config/zsh/env/omzrc.zsh
+++ roles/common/zsh/files/home/.config/zsh/env/omzrc.zsh
@@ -1,1 +1,5 @@
+# https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/history.zsh
 HISTFILE="$HOME/.config/zsh/.zsh_history"
+
+# https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/nvm#settings
+NVM_AUTOLOAD=1
